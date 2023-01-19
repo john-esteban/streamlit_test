afeaef97010d8369591d09cf76f89750cbb75180
john-esteban <john.esteban15@gmail.com>
2023-01-19 17:52:01 +0100

    update

--- api_connection.py
+++ api_connection.py
@@ -10,11 +10,10 @@ import numpy as np
 def get_data_from_api():
     
     url = "https://fifa-2022-schedule-and-stats.p.rapidapi.com/schedule"
+    params = {"date":"2022-11-22","utc_offset":"8"}
     headers = {
-	"X-RapidAPI-Key": "0cfd521736mshbc628a58783b056p161784jsn878d4847805f",
-	"X-RapidAPI-Host": "fifa-2022-schedule-and-stats.p.rapidapi.com"
-}
-    params = {"date":"2022-12-02","utc_offset":"8"}
+	"X-RapidAPI-Key": "8e6f634c5cmshf210e3a623f7435p1e10d5jsnd5433a37d688",
+	"X-RapidAPI-Host": "fifa-2022-schedule-and-stats.p.rapidapi.com"}
 
     response = requests.get(url, headers=headers, params=params)
     outputs = response.json()
